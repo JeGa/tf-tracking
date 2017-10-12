Fixed bug in total loss calculation.

networks/player_classificator.py
@@ -92,17 +92,16 @@ def _shapeinfo(cls_input, cls_target):
     step_size = input_shape[1]
     input_dimension = input_shape[2]
 
-    output_dimension = cls_target.get_shape()[2]
-
-    return batch_size, step_size, input_dimension, output_dimension
+    return batch_size, step_size, input_dimension
 
 
 def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001):
     """
     :param cls_input: Shape (batch_size, sequence_size, 40 * 3).
-    :param cls_target: Shape (batch_size, sequence_size, 10 + 1).
+    :param cls_target: Shape (batch_size, sequence_size, 10). NOTE: This means 10 classificators.
     """
-    batch_size, step_size, input_dimension, output_dimension = _shapeinfo(cls_input, cls_target)
+
+    batch_size, step_size, input_dimension = _shapeinfo(cls_input, cls_target)
 
     multirnn_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(state_size) for _ in range(num_layers)])
 
@@ -111,27 +110,58 @@ def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001
     # rnn_outputs is of shape (batch_size, step_size, state_size), final_state is a tuple with c_state and h_state.
     rnn_outputs, final_state = tf.nn.dynamic_rnn(multirnn_cell, cls_input, initial_state=init_state)
 
-    with tf.variable_scope('classificator'):
-        W = tf.get_variable('W', [state_size, output_dimension])
-        b = tf.get_variable('b', [output_dimension], initializer=tf.constant_initializer(0.0))
+    # Create classifier for each player.
+    PLAYERS = 10
+    output_dimension = PLAYERS + 1
+
+    all_W = []
+    all_b = []
+
+    for i in range(PLAYERS):
+        with tf.variable_scope('classificator_player_' + str(i)):
+            W = tf.get_variable('W', [state_size, output_dimension])
+            b = tf.get_variable('b', [output_dimension], initializer=tf.constant_initializer(0.0))
+
+            all_W.append(W)
+            all_b.append(b)
+
+    all_logits = []
 
     with tf.name_scope('classificator'):
-        all = tf.reshape(rnn_outputs, [-1, state_size])
-        out = tf.matmul(all, W) + b
+        for i in range(PLAYERS):
+            all = tf.reshape(rnn_outputs, [-1, state_size])
+            out = tf.matmul(all, all_W[i]) + all_b[i]
 
-        predictions = tf.reshape(out, [batch_size.value, step_size.value, output_dimension.value])
+            predictions = tf.reshape(out, [batch_size.value, step_size.value, output_dimension])
 
-    with tf.name_scope('loss'):
-        total_loss = tf.reduce_mean(
-            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predictions, labels=tf.squeeze(cls_target)))
+            all_logits.append(predictions)
+
+    all_players_loss = []
+
+    for i in range(PLAYERS):
+        with tf.name_scope('loss_player_' + str(i)):
+            # Shape (batch_size, sequence_size, 10 + 1).
+            predictions = all_logits[i]
+
+            # Shape (batch_size, sequence_size). Values from 0 to 10.
+            labels = cls_target[:, :, i]
+
+            player_loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predictions, labels=labels))
+
+            all_players_loss.append(player_loss)
+
+    total_loss = tf.add_n(all_players_loss)
 
     with tf.name_scope('training'):
         train_step = tf.train.AdagradOptimizer(learning_rate).minimize(total_loss)
 
-    tf.summary.histogram('cls_state_hist', rnn_outputs)
-    tf.summary.histogram('cls_prediction_hist', predictions)
+    # Make predictions.
+    all_predictions = []
+    for i in range(PLAYERS):
+        all_predictions.append(tf.nn.softmax(all_logits[i]))
 
-    return total_loss, train_step, tf.nn.softmax(predictions)
+    return total_loss, train_step, all_predictions
 
 
 def build(region_proposals, lstm_predictions, cls_target):

trainloop.py
@@ -4,6 +4,7 @@ import logging
 
 import util.helper
 import networks.player_classificator
+import util.global_config as global_config
 
 
 # If you want to run only the input data reading part.
@@ -32,18 +33,35 @@ def predict_frcnn(sequence_images, frcnn):
     return frcnn_out, frcnn_time.time()
 
 
-# TODO
 # Train the lstm with input data given as feed_dict.
-def train(sess, tensors, input_pipeline_out, frcnn_out):
+def train(sess, tensors, input_pipeline_out, frcnn_out, target_cls):
+    input_tensors = {
+        'train_step': tensors['combined']['train_step'],
+        'total_loss': tensors['combined']['loss'],
+
+        'cls_inputs': tensors['cls']['inputs'],
+        'cls_targets': tensors['cls']['targets'],
+        # This is a list.
+        'cls_predictions': tensors['cls']['predictions'],
+        # Shifted lstm pred. from 'lstm' part.
+        'last_lstm_predictions': tensors['cls']['last_lstm_predictions'],
+
+        'reg_targets': tensors['lstm']['targets'],
+
+        'summary': tensors['summary']
+    }
+
     with util.helper.timeit() as train_time:
-        train_lstm_out, summary_out = sess.run([tensors['lstm'], tensors['summary']], feed_dict={
+        out = sess.run(input_tensors, feed_dict={
             tensors['placeholders']['groundtruth_bbs']: input_pipeline_out['groundtruth_bbs'],
             tensors['placeholders']['target_bbs']: input_pipeline_out['target_bbs'],
             tensors['placeholders']['images']: input_pipeline_out['images'],
 
-            tensors['placeholders']['region_proposals']: frcnn_out
+            tensors['placeholders']['region_proposals']: frcnn_out,
+            tensors['placeholders']['target_cls']: target_cls
         })
-    return train_lstm_out, summary_out, train_time.time()
+
+    return out, train_time.time()
 
 
 # TODO
@@ -56,8 +74,8 @@ def interval_actions(epoch, step, globalstep,
             'Epoch %d, step %d, global step %d (%.3f/%.3f/%.3f sec input/frcnn_predict/train). Loss %.3f.' % (
                 epoch, step, globalstep, input_time, frcnn_time, train_time, loss))
 
-        # if step % global_config.cfg['summary_interval'] == 0:
-        #   train_writer.add_summary(summary, globalstep)
+    if step % global_config.cfg['summary_interval'] == 0:
+        train_writer.add_summary(summary, globalstep)
 
         # if step % global_config.cfg['result_interval'] == 0:
         #     util.helper.draw_bb_and_save(sequence_images * 255,
@@ -119,17 +137,26 @@ def run(sess, input_pipeline_tensors, input_handles, network_tensors,
             # Shape (batch_size, sequence_size, 10).
             target_cls = networks.player_classificator.generate_cls_gt(input_pipeline_out, frcnn_out)
 
+            # Draw the gt labels.
             # util.helper.draw_bb_and_cls_labels_and_save(input_pipeline_out['images'][0, 0],
             #                                             frcnn_out[0, 0],
             #                                             np.reshape(input_pipeline_out['target_bbs'][0, 0], (10, 4)),
             #                                             target_cls[0, 0],
-            #                                             '0_0')
+            #                                             '0_0_gt')
 
-            lstm_out, summary_out, train_time = train(sess, network_tensors, input_pipeline_out, frcnn_out)
+            out, train_time = train(sess, network_tensors, input_pipeline_out, frcnn_out, target_cls)
 
             interval_actions(epoch, step, globalstep,
                              input_time, frcnn_time, train_time,
-                             lstm_out['loss'], train_writer, summary_out)
+                             out['total_loss'], train_writer, out['summary'])
+
+            # Draw the predicted classification labels.
+            # util.helper.draw_allbbs_and_cls_labels_and_save(np.reshape(out['reg_targets'][0, 0], (10, 4)),
+            #                                                 out['last_lstm_predictions'][0, 0],
+            #                                                 frcnn_out[0, 0],
+            #                                                 out['cls_targets'][0, 0],
+            #                                                 out['cls_predictions'][0, 0],
+            #                                                 '0_0_pred')
 
             step += 1
             globalstep += 1

util/builder.py
@@ -48,7 +48,9 @@ def bbdata_image(bbs, image, name):
 
 
 def add_summaries(tensors):
-    tf.summary.scalar('lstm_loss', tensors['lstm']['loss'])
+    tf.summary.scalar('lstm_reg_loss', tensors['lstm']['loss'])
+    tf.summary.scalar('lstm_cls_loss', tensors['cls']['loss'])
+    tf.summary.scalar('total_loss', tensors['combined']['loss'])
 
     batch = 0
     sequence_element = -1
@@ -84,7 +86,7 @@ def build_network(inputs, targets, region_proposals, target_cls):
             target_cls)
 
     with tf.name_scope('total_loss'):
-        total_loss = 0.5 * lstm_cls_total_loss + 0.5 * lstm_cls_total_loss
+        total_loss = 0.5 * lstm_cls_total_loss + 0.5 * lstm_reg_total_loss
 
     with tf.name_scope('total_loss_training'):
         total_train_step = tf.train.AdagradOptimizer(global_config.cfg['learning_rate']).minimize(total_loss)