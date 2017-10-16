Add variables for weighting. Fixed reg weights.

networks/cls_lstm.py
@@ -95,20 +95,25 @@ def _shapeinfo(cls_input, cls_target):
     return batch_size, step_size, input_dimension
 
 
-def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001):
+def network(cls_input, cls_target, state_size, num_layers=1):
     """
     :param cls_input: Shape (batch_size, sequence_size, 40 * 3).
     :param cls_target: Shape (batch_size, sequence_size, 10). NOTE: This means 10 classificators.
     """
-
     batch_size, step_size, input_dimension = _shapeinfo(cls_input, cls_target)
 
-    multirnn_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(state_size) for _ in range(num_layers)])
+    multirnn_cell_fw = tf.nn.rnn_cell.MultiRNNCell(
+        [tf.nn.rnn_cell.LSTMCell(state_size, use_peepholes=True) for _ in range(num_layers)])
+    multirnn_cell_bw = tf.nn.rnn_cell.MultiRNNCell(
+        [tf.nn.rnn_cell.LSTMCell(state_size, use_peepholes=True) for _ in range(num_layers)])
 
-    init_state = multirnn_cell.zero_state(batch_size, dtype=tf.float32)
+    # rnn_outputs = (output_fw, output_bw), each with shape (batch_size, step_size, state_size).
+    # final_states = (output_state_fw, output_state_bw), each a tuple with c_state and h_state.
+    rnn_outputs, final_states = tf.nn.bidirectional_dynamic_rnn(multirnn_cell_fw, multirnn_cell_bw, cls_input,
+                                                                dtype=tf.float32)
 
-    # rnn_outputs is of shape (batch_size, step_size, state_size), final_state is a tuple with c_state and h_state.
-    rnn_outputs, final_state = tf.nn.dynamic_rnn(multirnn_cell, cls_input, initial_state=init_state)
+    # Shape (batch_size, step_size, 2 * state_size).
+    rnn_outputs = tf.concat(rnn_outputs, axis=2)
 
     # Create classifier for each player.
     PLAYERS = 10
@@ -119,7 +124,7 @@ def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001
 
     for i in range(PLAYERS):
         with tf.variable_scope('classificator_player_' + str(i)):
-            W = tf.get_variable('W', [state_size, output_dimension])
+            W = tf.get_variable('W', [2 * state_size, output_dimension])
             b = tf.get_variable('b', [output_dimension], initializer=tf.constant_initializer(0.0))
 
             all_W.append(W)
@@ -129,7 +134,7 @@ def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001
 
     with tf.name_scope('classificator'):
         for i in range(PLAYERS):
-            all = tf.reshape(rnn_outputs, [-1, state_size])
+            all = tf.reshape(rnn_outputs, [-1, 2 * state_size])
             out = tf.matmul(all, all_W[i]) + all_b[i]
 
             predictions = tf.reshape(out, [batch_size.value, step_size.value, output_dimension])
@@ -153,24 +158,24 @@ def network(cls_input, cls_target, state_size, num_layers=1, learning_rate=0.001
 
     total_loss = tf.add_n(all_players_loss)
 
-    with tf.name_scope('training'):
-        train_step = tf.train.AdagradOptimizer(learning_rate).minimize(total_loss)
+    # TODO
+    # with tf.name_scope('training'):
+    #   train_step = tf.train.AdagradOptimizer(learning_rate).minimize(total_loss)
 
     # Make predictions.
     all_predictions = []
     for i in range(PLAYERS):
         all_predictions.append(tf.nn.softmax(all_logits[i]))
 
-    return total_loss, train_step, all_predictions
+    return total_loss, all_predictions
 
 
 def build(region_proposals, lstm_predictions, cls_target):
     cls_input, last_region_proposals, last_lstm_predictions = arrange_classifier_inputs(region_proposals,
                                                                                         lstm_predictions)
 
-    total_loss, train_step, predictions = network(cls_input, cls_target,
-                                                  global_config.cfg['lstm_cls_state_size'],
-                                                  global_config.cfg['lstm_cls_layers'],
-                                                  global_config.cfg['lstm_cls_learning_rate'])
+    total_loss, predictions = network(cls_input, cls_target,
+                                      global_config.cfg['lstm_cls_state_size'],
+                                      global_config.cfg['lstm_cls_layers'])
 
-    return cls_input, last_region_proposals, last_lstm_predictions, total_loss, train_step, predictions
+    return cls_input, last_region_proposals, last_lstm_predictions, total_loss, predictions

trainloop.py
@@ -4,8 +4,13 @@ import logging
 import os
 
 import util.helper
-import networks.player_classificator
+import networks.cls_lstm
 import util.global_config as global_config
+import valloop
+
+# TODO: Hacky, Hacky.
+frcnn_out = None
+frcnn_time = None
 
 
 # If you want to run only the input data reading part.
@@ -37,7 +42,8 @@ def predict_frcnn(sequence_images, frcnn):
 
 
 # Train the lstm with input data given as feed_dict.
-def train(sess, tensors, input_pipeline_out, frcnn_out, target_cls):
+def train(sess, tensors, input_pipeline_out, frcnn_out, target_cls,
+          cls_weight=0.5, reg_weight=0.5):
     input_tensors = {
         'train_step': tensors['combined']['train_step'],
         'total_loss': tensors['combined']['loss'],
@@ -62,7 +68,10 @@ def train(sess, tensors, input_pipeline_out, frcnn_out, target_cls):
             tensors['placeholders']['images']: input_pipeline_out['images'],
 
             tensors['placeholders']['region_proposals']: frcnn_out,
-            tensors['placeholders']['target_cls']: target_cls
+            tensors['placeholders']['target_cls']: target_cls,
+
+            tensors['combined']['cls_weight']: cls_weight,
+            tensors['combined']['reg_weight']: reg_weight
         })
 
     return out, train_time.time()
@@ -70,30 +79,31 @@ def train(sess, tensors, input_pipeline_out, frcnn_out, target_cls):
 
 def interval_actions(epoch, step, globalstep,
                      input_time, frcnn_time, train_time,
-                     loss, train_writer, summary, saver, sess):
+                     loss, train_writer, summary, saver, sess, validate=True):
     # sequence_images, out_input_pipeline, out_frcnn, out_lstm):
-    if step % 1 == 0:
+    if globalstep % 1 == 0:
         logging.info(
             'Epoch %d, step %d, global step %d (%.3f/%.3f/%.3f sec input/frcnn_predict/train). Loss %.3f.' % (
                 epoch, step, globalstep, input_time, frcnn_time, train_time, loss))
 
-    if step % global_config.cfg['summary_interval'] == 0:
+    if globalstep % global_config.cfg['summary_interval'] == 0:
         train_writer.add_summary(summary, globalstep)
 
-    if step % global_config.cfg['result_interval'] == 0:
+    if globalstep % global_config.cfg['result_interval'] == 0:
         pass
 
-    if step % global_config.cfg['save_interval'] == 0:
+    if globalstep % global_config.cfg['save_interval'] == 0:
         saver.save(sess, os.path.join(global_config.cfg['checkpoints'], 'checkpoint'),
                    global_step=globalstep)
 
-        # if validate:
-        #    if globalstep % global_config.cfg['validation_interval'] == 0:
-        #        validation_loop(sess, tensors, input_handles, train_writer, globalstep)
+    if validate:
+        if globalstep % global_config.cfg['validation_interval'] == 0:
+            valloop.run()  # (sess, tensors, input_handles, train_writer, globalstep)
 
 
 def run(sess, input_pipeline_tensors, input_handles, network_tensors,
-        train_writer, epoch, saver, globalstep, frcnn, validate=True):
+        train_writer, epoch, saver, globalstep, frcnn,
+        cls_weight, reg_weight, validate=True):
     """
     input_pipeline_tensors.keys():
         'groundtruth_bbs'
@@ -131,19 +141,19 @@ def run(sess, input_pipeline_tensors, input_handles, network_tensors,
         try:
             input_pipeline_out, input_time = read_input(sess, input_pipeline_tensors, input_handles)
 
-            frcnn_out, frcnn_time = predict_frcnn(input_pipeline_out['images'], frcnn)
+            global frcnn_out
+            global frcnn_time
+
+            if frcnn_out is None:
+                frcnn_out, frcnn_time = predict_frcnn(input_pipeline_out['images'], frcnn)
+            else:
+                frcnn_time = 0
 
             # Shape (batch_size, sequence_size, 10).
-            target_cls = networks.player_classificator.generate_cls_gt(input_pipeline_out, frcnn_out)
+            target_cls = networks.cls_lstm.generate_cls_gt(input_pipeline_out, frcnn_out)
 
-            # Draw the gt labels.
-            # util.helper.draw_bb_and_cls_labels_and_save(input_pipeline_out['images'][0, 0],
-            #                                             frcnn_out[0, 0],
-            #                                             np.reshape(input_pipeline_out['target_bbs'][0, 0], (10, 4)),
-            #                                             target_cls[0, 0],
-            #                                             '0_0_gt')
-
-            out, train_time = train(sess, network_tensors, input_pipeline_out, frcnn_out, target_cls)
+            out, train_time = train(sess, network_tensors, input_pipeline_out, frcnn_out, target_cls,
+                                    cls_weight, reg_weight)
 
             interval_actions(epoch, step, globalstep,
                              input_time, frcnn_time, train_time,
@@ -157,7 +167,7 @@ def run(sess, input_pipeline_tensors, input_handles, network_tensors,
                 return classifications
 
             # Draw the predicted classification labels.
-            if step % 5 == 0:
+            if globalstep % 5 == 0:
                 for s in range(input_pipeline_out['images'].shape[1]):
                     util.helper.draw_allbbs_and_cls_labels_and_save(
                         input_pipeline_out['images'][0, s],

util/builder.py
@@ -92,7 +92,11 @@ def build_network(inputs, targets, region_proposals, target_cls):
         total_loss = cls_weight * lstm_cls_total_loss + reg_weight * lstm_reg_total_loss
 
     with tf.name_scope('total_loss_training'):
-        total_train_step = tf.train.AdagradOptimizer(global_config.cfg['learning_rate']).minimize(total_loss)
+        regvars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'model/lstm_bb_regressor')
+        clsvars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'model/lstm_bb_classificator')
+
+        total_train_step = tf.train.AdagradOptimizer(global_config.cfg['learning_rate']).minimize(total_loss,
+                                                                                                  var_list=[clsvars])
 
     lstm_tensors = {
         'inputs': inputs,

util/helper.py
@@ -134,8 +134,8 @@ def draw_allbbs_and_cls_labels_and_save(
         gt_bb = reg_targets[i]
 
         xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(gt_bb)
-        draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='red')
-        draw.text([xmin * width, ymin * height - 10], str(i + 1), fill='red')
+        # draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='red')
+        # draw.text([xmin * width, ymin * height - 10], str(i + 1), fill='red')
 
     # rps.
     for i in range(players):
@@ -151,35 +151,35 @@ def draw_allbbs_and_cls_labels_and_save(
         # gt_label = findlabel(cls_targets, i)
 
         xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(rp_bb)
-        draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='green')
-        draw.text([xmin * width + 20, ymin * height - 10], str(i + 1) + '-', fill='green')  # + str(int(gt_label)
+        # draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='green')
+        # draw.text([xmin * width + 20, ymin * height - 10], str(i + 1) + '-', fill='green')  # + str(int(gt_label)
 
     # lstm
-    # for i in range(players):
-    #     pred_bb = reg_predictions[i]
-    #
-    #     xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(pred_bb)
-    #     draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='blue')
-    #
-    # # The predicted bb.
-    # for i in range(players):
-    #     # i = player id.
-    #
-    #     pred_label = np.argmax(cls_predictions[i])
-    #
-    #     # Means: For player i take rp pred_label (if != 0) or take i-th lstm prediction.
-    #
-    #     # Take lstm prediction from player i.
-    #     if pred_label == 0:
-    #         xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(reg_predictions[i])
-    #         draw.rectangle([xmin * width - 1, ymin * height - 1, xmax * width + 1, ymax * height + 1], outline='yellow')
-    #         draw.text([xmin * width + 40, ymin * height - 10], str(i + 1), fill='yellow')
-    #     # Take rp pred_label.
-    #     else:
-    #         rp_bb = region_proposals[pred_label - 1]
-    #         draw.rectangle([rp_bb[1] * width - 1, rp_bb[0] * height - 1, rp_bb[3] * width + 1, rp_bb[2] * height + 1],
-    #                        outline='yellow')
-    #         draw.text([rp_bb[1] * width + 40, rp_bb[0] * height - 10], str(i), fill='yellow')
+    for i in range(players):
+        pred_bb = reg_predictions[i]
+
+        # xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(pred_bb)
+        # draw.rectangle([xmin * width, ymin * height, xmax * width, ymax * height], outline='blue')
+
+    # The predicted bb.
+    for i in range(players):
+        # i = player id.
+
+        pred_label = np.argmax(cls_predictions[i])
+
+        # Means: For player i take rp pred_label (if != 0) or take i-th lstm prediction.
+
+        # Take lstm prediction from player i.
+        if pred_label == 0:
+            xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(reg_predictions[i])
+            draw.rectangle([xmin * width - 1, ymin * height - 1, xmax * width + 1, ymax * height + 1], outline='yellow')
+            draw.text([xmin * width + 40, ymin * height - 10], str(i + 1), fill='yellow')
+        # Take rp pred_label.
+        else:
+            rp_bb = region_proposals[pred_label - 1]
+            xmin, ymin, xmax, ymax = xywh_to_xmin_ymin_xmax_ymax(rp_bb)
+            draw.rectangle([xmin * width - 1, ymin * height - 1, xmax * width + 1, ymax * height + 1], outline='yellow')
+            draw.text([xmin * width + 40, ymin * height - 10], str(i + 1) + '-' + str(pred_label), fill='yellow')
 
     draw.text([10, 10], 'groundtruth_bb', fill='red')
     draw.text([10, 20], 'region_proposal', fill='green')